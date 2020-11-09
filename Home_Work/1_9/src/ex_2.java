047bb1e564e5631e6e5080b48e48dbd63aa5c436
ReddyIAm <72807034+ReddyIAm@users.noreply.github.com>
2020-11-09 17:15:42 +0300

    Adding new exercises

--- Home_Work/1_9/src/ex_2.java
+++ Home_Work/1_9/src/ex_2.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class ex_2 {
-    public static int GetSumOfNum(int x){
-        return x % 10 + x / 10;
-    }
-    public static void Writeln(int x){
+    public static void Writeln(int x) {
         System.out.println(x);
+    }
+        public static void GetSumOfNum(){
+        int sum = 0;
+        for(int i = 10; i < 100; i++){
+            if(i % 7 == 0){
+                sum+= i % 10 + i / 10;
+            }
+        }
+        Writeln(sum);
     }
     public static void main(String args[]){
         Scanner In = new Scanner(System.in);
-        int x = In.nextInt();
-        Writeln(GetSumOfNum(x));
+        int sum = 0;
+        GetSumOfNum();
     }
 }
